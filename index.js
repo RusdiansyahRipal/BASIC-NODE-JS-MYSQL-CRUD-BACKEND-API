b25dd621fb866b510895d868fd675a4cb72283d8
RusdiansyahRipal <rusdiansyahripal@gmail.com>
2026-04-29 22:09:12 +0700

    membuat code untuk mencari data melalui route get dan mengimplementasikan pencegahan sql injection

--- index.js
+++ index.js
@@ -11,11 +11,39 @@ app.get("/", (req, res) => {
   response( 200, "ini data ", "ini pesan " , res )
 })
 
+//get all
 app.get("/mahasiswa",(req,res)=>{
   const sql = "SELECT * from mahasiswa"
   db.query(sql,(err,field)=>{
+    if (err) {
+    return response(500, null, err.message, res)
+  }
    response(200,field,"cek aja", res)
   })  
+ })
+//get by parameter
+ app.get("/mahasiswa/:nim",(req,res)=>{
+  const nim = req.params.nim;
+
+  //validasi harus angka yang dimasukan kedalam paramameter route
+  if(isNaN(nim)){
+    return response(400,null,"harus angka yang dikirim", res)
+  }
+
+  const sql = `SELECT * FROM mahasiswa WHERE nim = ?`
+  db.query(sql,[nim],(err,field) => {
+
+    // logika jika error
+    if(err){
+      return response(500,null, err.message, res)
+    }
+    // logika jika nim sama tidak ada
+    if(field.length === 0 ) {
+      return(404, null, "data tidak ditemukan", 0)
+    }
+    response(200,field,"mengambil data melalui parameter route", res)
+  })
+
  })
 
 app.put("/",(req,res)=>{})
